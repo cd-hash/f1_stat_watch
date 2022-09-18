94608fadd1daf129bb2d1aafa061912bc3b1e6b9
chris <cdresher2010@gmail.com>
2022-09-18 08:34:24 -0400

    added all endpoints I need and wrote get_most_recent_race now I just need a way to backfill

--- lib/stat_watch.ex
+++ lib/stat_watch.ex
@@ -1,10 +1,15 @@
 defmodule StatWatch do
+  import SweetXml
   @moduledoc """
   Documentation for `StatWatch`.
   """
 
   def column_names do
     Enum.join(~w(Year Round Driver Q1 Q2 Q3), ",")
+  end
+
+  def recent_race_endpoint do
+    "http://ergast.com/api/f1/current/last/results"
   end
 
   def quali_times_endpoint(round, year \\ 2021) do
@@ -14,6 +19,15 @@ defmodule StatWatch do
   def season_schedule_endpoint(year) do
     "http://ergast.com/api/f1/#{year}"
   end
+
+  def get_most_recent_race do
+    case HTTPoison.get(recent_race_endpoint()) do
+        {:ok, response} -> response.body |> SweetXml.xpath(~x"//RaceTable/@*"l) #[year, round]
+        {:error, reason} -> {:error, reason}
+    end
+  end
+
+# [year, round] = get_most_recent_race()
 
   def get_quali_times(round, year) do
     HTTPoison.get(quali_times_endpoint(round, year))
